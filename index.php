9cec0c6f6dca765806746e15ea976acbfa36a3c5
xmaison2 <a1exandrer0cha.ti@gmail.com>
2014-02-15 17:12:45 -0200

    Menu Superior

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Corpo & Saúde</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+<div class="off-canvas-wrap">
+  <div class="inner-wrap">
+    <nav class="tab-bar">
+      <section class="left-small">
+        <a class="left-off-canvas-toggle menu-icon" ><span></span></a>
+      </section>
+
+      <section class="middle tab-bar-section">
+        <h1 class="title">Corpo & Saúde</h1>
+      </section>
+    </nav>
+
+    <aside class="left-off-canvas-menu">
+      <ul class="off-canvas-list">
+        <li><label>Histórico</label></li>
+        <li><a href="#">Séries</a></li>
+        <li><a href="#">Dieta</a></li>
+        <li><a href="#">Resultados</a></li>
+        
+        
+      </ul>
+    </aside>
+
+    <section class="main-section">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra ornare neque sed tempor. Donec sed laoreet ligula. Praesent venenatis sodales consequat. Cras consectetur tempus lorem, eu varius dolor. Ut rutrum sapien purus, id lacinia turpis tincidunt non. Sed ac dictum justo, et dapibus mi. In ac ornare diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam et aliquet ante. Cras faucibus euismod mauris ut dapibus. Etiam sed ante risus. Nam venenatis lacus sed pretium fringilla. Nam varius lacus ac nulla convallis aliquam.
+
+Etiam euismod erat vel nisi tincidunt volutpat. Maecenas eu odio ac arcu ullamcorper accumsan id luctus leo. In feugiat et diam vitae varius. Vestibulum turpis dui, scelerisque convallis lacinia at, feugiat eget nunc. Aenean a vestibulum massa. Morbi nulla libero, luctus cursus placerat ut, scelerisque quis arcu. Suspendisse elementum eros at nisi fringilla, in posuere magna dignissim. Integer sodales gravida dolor et dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum varius fringilla enim. Pellentesque mattis enim sit amet neque sodales, vitae placerat lacus pellentesque. Nullam auctor est convallis orci blandit interdum.
+
+Praesent tempus, felis ut faucibus rutrum, velit urna varius turpis, eget consequat sem dolor sed ligula. Phasellus dictum risus nibh, nec laoreet orci ornare ac. Etiam non justo ac magna condimentum dictum. Maecenas ipsum diam, vulputate sed varius vestibulum, sagittis ac nulla. Morbi justo orci, blandit id molestie ut, rhoncus quis massa. Fusce ornare, eros vitae malesuada blandit, nisl elit scelerisque est, ut varius velit neque ut est. Aenean ut elit sed neque mollis iaculis. Praesent sit amet lorem tincidunt, congue felis in, euismod arcu.
+
+Cras sit amet adipiscing tellus. Sed et dui hendrerit, consectetur metus mattis, fringilla dolor. In placerat tortor eget luctus consectetur. Integer molestie dapibus leo, non vehicula purus vulputate eget. Proin rhoncus est nec ante ultricies rhoncus. Maecenas id dapibus lorem, a ultricies purus. Mauris non lectus vel augue sodales pretium. Integer magna sem, sollicitudin id hendrerit in, porttitor eget augue. Etiam gravida enim quis tellus tincidunt, at sodales turpis posuere. Aenean urna ipsum, dignissim sit amet elit vel, rutrum dapibus erat. Duis a pellentesque eros, a interdum elit. Praesent mattis ligula purus, vitae congue massa imperdiet eget. Donec tempor velit ut nisl semper iaculis. Donec sit amet enim a quam lacinia dignissim in non ante.
+
+Aliquam ut tempor lectus, vel bibendum tortor. Nam laoreet tristique mi ut semper. Donec quis aliquam nunc. Sed in enim sodales, aliquam odio ac, fermentum urna. Maecenas luctus felis est, sit amet condimentum massa blandit id. Mauris placerat diam sit amet libero faucibus, eu pulvinar tellus eleifend. Praesent libero velit, dapibus vitae arcu non, pellentesque mollis nisl. Cras vehicula magna ac eros tincidunt, eget ornare nulla malesuada. Nam ornare tempus urna, ac vestibulum erat viverra quis. Duis hendrerit volutpat quam eget luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque sodales eu turpis iaculis ullamcorper. Etiam vel mauris id risus ullamcorper porttitor. Sed adipiscing massa ut libero ultrices, eu accumsan mauris ullamcorper.
+    </section>
+
+  <a class="exit-off-canvas"></a>
+
+  </div>
+</div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
